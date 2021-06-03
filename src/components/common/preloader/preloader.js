b3639f4884f645af7bab57c869aea9386beb64bb
kstepanyan111 <you@example.com>
2021-06-03 21:58:57 +0400

    video 59

---
+++ src/components/common/preloader/preloader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import preloader from "./../../../assets/images/preloader.png"
+
+
+const Preloader = (props) => {
+    return<>
+    <div>
+        <img src={preloader}/>
+    </div>
+    </>
+}
+
+export default Preloader;
